d06b7fef5e45ae7078263edb8d986f0e3c05588b
Navarro <joseanp0202@gmail.com>
2024-12-08 12:00:44 +0000

    Apartado 7

--- src/main/kotlin/WaterCounter.kt
+++ src/main/kotlin/WaterCounter.kt
@@ -11,26 +11,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun WaterCounter (modifier: Modifier = Modifier) {
-    Column (
-        modifier = modifier.padding(16.dp)
-    ) {
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
         if (count > 0) {
-            var showTask by remember { mutableStateOf(true) }
-            if (showTask) {
-                WellnessTaskItem(
-                    onClose = { },
-                    taskName = "Have you taken your 15 minute walk today?"
-                )
-            }
             Text("You've had $count glasses.")
         }
-        Button (
-            onClick = { count++ },
-            modifier = modifier.padding(top = 8.dp),
-            enabled = count < 10
-        ) {
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
             Text("Añadir 1")
         }
     }
